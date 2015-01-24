61140b836ccec00611238cd4476b987a870b3e0d
Roy Zheng <roycraft3@gmail.com>
2015-01-24 10:12:49 -0800

    network fix

--- game/Assets/network.cs
+++ game/Assets/network.cs
@@ -10,20 +10,21 @@ public class network : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-		
 				//		MasterServer.ipAddress="192.168.59.3";
 				//		MasterServer.port = 23466;
 				//		Network.natFacilitatorIP = "192.168.59.3";
 				//		Network.natFacilitatorPort=50005;
 				if (Network.peerType == NetworkPeerType.Disconnected) {
 						Debug.Log ("in connect");
+						Network.Disconnect ();
+						MasterServer.UnregisterHost ();
 						MasterServer.RequestHostList (MASTERSERVER_ID);
 						HostData[] hostDataArray = MasterServer.PollHostList ();
 						Debug.Log (hostDataArray.ToString ());
 						if (hostDataArray.Length == 0) {
 								string id = System.Guid.NewGuid ().ToString ();
 								Network.InitializeServer (8, portNum, !Network.HavePublicAddress ());
-								MasterServer.RegisterHost (MASTERSERVER_ID, id, "come join");
+								MasterServer.RegisterHost (MASTERSERVER_ID, id);
 								Debug.Log ("No servers found, created new server with id " + id);
 						} else {
 								Network.Connect (hostDataArray [0]);
