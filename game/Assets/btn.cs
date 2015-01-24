d0a6b242825426120274793218a3e35b4db81a20
Akhilesh Saklecha <saklecha@usc.edu>
2015-01-24 14:30:00 -0800

    pushing up prefabs annd such

---
+++ game/Assets/btn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void onClick () {
+		int count = 0;
+		Debug.Log("hello"+count++);
+	}
+}
---
+++ game/Assets/gyroTest.js
@@ -0,0 +1,16 @@
+﻿ var dir:Vector3;
+var style:GUIStyle;
+function Awake()
+{
+style=new GUIStyle();
+style.alignment=TextAnchor.MiddleCenter;
+style.normal.textColor=Color.red;
+}
+function Update()
+{
+dir=Input.acceleration;
+}
+function OnGUI()
+{
+GUI.Label(Rect(Screen.width/2-100,Screen.height/2-100,200,200),dir+"",style);
+}
