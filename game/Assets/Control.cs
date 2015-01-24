54b59f92c1c576ca61546ad9314699185d18db25
Akhilesh Saklecha <saklecha@usc.edu>
2015-01-24 11:36:20 -0800

    Controls final

--- game/Assets/Control.cs
+++ game/Assets/Control.cs
@@ -2,17 +2,43 @@
 using System.Collections;
 
 public class Control : MonoBehaviour {
-
+	private int shift;
+	public int speed;
 	// Use this for initialization
 	void Start () {
-
+		shift=0;
+		speed = 600;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		int speed;
-		speed = 60;
-		Vector3 movement = new Vector3 ( 0,0,(Input.acceleration.y * speed));
-		rigidbody.AddForce (movement);
+
+		if(Input.acceleration.x>0)
+		{
+			if(shift>0)
+			{
+				rigidbody.velocity=Vector3.zero;
+				rigidbody.angularVelocity=Vector3.zero;
+			}
+			Vector3 movement = new Vector3 ( (speed*Time.deltaTime),0,0);
+			rigidbody.velocity=movement;
+			shift=-1;
+		}
+		else if(Input.acceleration.x<0)
+		{
+			if(shift<0)
+			{
+				rigidbody.velocity=Vector3.zero;
+				rigidbody.angularVelocity=Vector3.zero;
+			}
+			Vector3 movement = new Vector3 ( (-1*speed*Time.deltaTime),0,0);
+			rigidbody.velocity=movement;
+			shift=1;
+		}
+		else
+		{
+			rigidbody.velocity=Vector3.zero;
+			rigidbody.angularVelocity=Vector3.zero;
+		}
 	}
 }
