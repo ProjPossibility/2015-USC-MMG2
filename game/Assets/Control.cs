8a0ea9f57d09289cd82a6495b98dfd26844b70fd
Roy Zheng <roycraft3@gmail.com>
2015-01-25 00:31:18 -0800

    fuck its late

--- game/Assets/Control.cs
+++ game/Assets/Control.cs
@@ -7,14 +7,21 @@ public class Control : MonoBehaviour
 		public float x_limit = 5;
 	public int speed=500;
 		private float deadzone;
+	private bool stop=false;
 		// Use this for initialization
 		void Start ()
 		{
+		this.stop = false;
 		} 
-	
+	public void Stop(){
+		this.stop = true;
+		rigidbody.velocity = Vector3.zero;
+	}
 		// Update is called once per frame
 		void Update ()
 		{
+		if (this.stop)
+						return;
 				if (Input.GetKey (KeyCode.D) || Input.acceleration.x > deadzone_width / 2) {
 						if (rigidbody.transform.position.x > x_limit) {
 								rigidbody.velocity = Vector3.zero;
--- game/Assets/collision.cs
+++ game/Assets/collision.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class collision : MonoBehaviour {
 
 	private int count;
-	public bool showMsg;
 	public collision collisionScript;
 	public Control controlScript;
 	public NetworkView network;
@@ -15,7 +14,6 @@ public class collision : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		count = 0;
-		showMsg = false;
 	}
 	
 	// Update is called once per frame
@@ -24,30 +22,23 @@ public class collision : MonoBehaviour {
 	}
 	[RPC]
 	void endGame(){
+		Debug.Log ("in end game");
 		GameObject.Instantiate (this.lost ? this.defeat_gui : this.victory_gui);
 	}
 	void OnCollisionEnter(Collision col)
 	{
-		showMsg = true;
 		Debug.Log ("in on collision");
 		if (col.gameObject.name == "Cube")
 						Debug.Log ("collision with cube " + this.count++);
 		this.lost = true;
 		this.network.RPC ("endGame", RPCMode.All);
+		this.collider.isTrigger = true;
+		controlScript = this.GetComponent<Control>();
+		controlScript.Stop();
+		controlScript.enabled = false;
 	}
 	void OnGUI()
 	{
-		GUIStyle msgStlye = new GUIStyle();
-		msgStlye.normal.textColor=Color.black;
-		if(showMsg)
-		{
-			GUI.Label (new Rect (0,0,Screen.width / 2,Screen.height/2), "YOU LOSE",msgStlye);
-			GameObject 	player= GameObject.Find("player");
-			player.collider.isTrigger = true;
-			controlScript = player.GetComponent<Control>();
-			controlScript.enabled = false;
-		}
-
 
 	}
 }
