c137b609f783d5c2621fcb59ad6d4f939fd71d51
Roy Zheng <roycraft3@gmail.com>
2015-01-25 00:35:57 -0800

    fixes

--- game/Assets/collision.cs
+++ game/Assets/collision.cs
@@ -1,44 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class collision : MonoBehaviour {
+public class collision : MonoBehaviour
+{
 
-	private int count;
-	public collision collisionScript;
-	public Control controlScript;
-	public NetworkView network;
-	public GameObject victory_gui;
-	public GameObject defeat_gui;
-
-	bool lost = false;
-	// Use this for initialization
-	void Start () {
-		count = 0;
-	}
+		private int count;
+		public collision collisionScript;
+		public NetworkView network;
+		public GameObject victory_gui;
+		public GameObject defeat_gui;
+		private bool end = false;
+		bool lost = false;
+		// Use this for initialization
+		void Start ()
+		{
+				count = 0;
+				end = false;
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
-	[RPC]
-	void endGame(){
-		Debug.Log ("in end game");
-		GameObject.Instantiate (this.lost ? this.defeat_gui : this.victory_gui);
-	}
-	void OnCollisionEnter(Collision col)
-	{
-		Debug.Log ("in on collision");
-		if (col.gameObject.name == "Cube")
+		}
+		[RPC]
+		void endGame ()
+		{
+				end = true;
+				Debug.Log ("in end game");
+				GameObject.Instantiate (this.lost ? this.defeat_gui : this.victory_gui);
+		}
+		void OnCollisionEnter (Collision col)
+		{
+				if (end)
+						return;
+				Debug.Log ("in on collision");
+				if (col.gameObject.name == "Cube")
 						Debug.Log ("collision with cube " + this.count++);
-		this.lost = true;
-		this.network.RPC ("endGame", RPCMode.All);
-		this.collider.isTrigger = true;
-		controlScript = this.GetComponent<Control>();
-		controlScript.Stop();
-		controlScript.enabled = false;
-	}
-	void OnGUI()
-	{
+				this.lost = true;
+				this.network.RPC ("endGame", RPCMode.All);
+				this.collider.isTrigger = true;
+				Control controlScript = this.GetComponent<Control> ();
+				controlScript.Stop ();
+				controlScript.enabled = false;
+		}
+		void OnGUI ()
+		{
 
-	}
+		}
 }
