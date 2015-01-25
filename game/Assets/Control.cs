6daa454924dc9058c258ea84f8d801e64c4c0421
Roy Zheng <roycraft3@gmail.com>
2015-01-24 17:23:20 -0800

    astyle

--- game/Assets/Control.cs
+++ game/Assets/Control.cs
@@ -1,44 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Control : MonoBehaviour {
-	private int shift;
-	public int speed;
-	// Use this for initialization
-	void Start () {
-		shift=0;
-		speed = 600;
-	}
+public class Control : MonoBehaviour
+{
+		private int shift;
+		public int speed;
+		// Use this for initialization
+		void Start ()
+		{
+				shift = 0;
+				speed = 600;
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 
-		if(Input.acceleration.x>0)
-		{
-			if(shift>0)
-			{
-				rigidbody.velocity=Vector3.zero;
-				rigidbody.angularVelocity=Vector3.zero;
-			}
-			Vector3 movement = new Vector3 ( (speed*Time.deltaTime),0,0);
-			rigidbody.velocity=movement;
-			shift=-1;
+				if (Input.acceleration.x > 0) {
+						if (shift > 0) {
+								rigidbody.velocity = Vector3.zero;
+								rigidbody.angularVelocity = Vector3.zero;
+						}
+						Vector3 movement = new Vector3 ((speed * Time.deltaTime), 0, 0);
+						rigidbody.velocity = movement;
+						shift = -1;
+				} else if (Input.acceleration.x < 0) {
+						if (shift < 0) {
+								rigidbody.velocity = Vector3.zero;
+								rigidbody.a ngularVelocity = Vector3.zero;
+						}
+						Vector3 movement = new Vector3 ((-1 * speed * Time.deltaTime), 0, 0);
+						rigidbody.velocity = movement;
+						shift = 1;
+				} else {
+						rigidbody.velocity = Vector3.zero;
+						rigidbody.angularVelocity = Vector3.zero;
+				}
 		}
-		else if(Input.acceleration.x<0)
-		{
-			if(shift<0)
-			{
-				rigidbody.velocity=Vector3.zero;
-				rigidbody.angularVelocity=Vector3.zero;
-			}
-			Vector3 movement = new Vector3 ( (-1*speed*Time.deltaTime),0,0);
-			rigidbody.velocity=movement;
-			shift=1;
-		}
-		else
-		{
-			rigidbody.velocity=Vector3.zero;
-			rigidbody.angularVelocity=Vector3.zero;
-		}
-	}
 }
