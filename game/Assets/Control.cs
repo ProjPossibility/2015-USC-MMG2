19012f4ac06fd221d8a0614b7f8189685a62cfda
Roy Zheng <roycraft3@gmail.com>
2015-01-24 18:06:53 -0800

    combiend

--- game/Assets/Control.cs
+++ game/Assets/Control.cs
@@ -16,14 +16,14 @@ public class Control : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				if (Input.GetKeyDown (KeyCode.D) || Input.acceleration.x > deadzone_width / 2) {
+				if (Input.GetKey (KeyCode.D) || Input.acceleration.x > deadzone_width / 2) {
 						if (rigidbody.transform.position.x > x_limit) {
 								rigidbody.velocity = Vector3.zero;
 								return;
 						}
 						Vector3 movement = new Vector3 ((speed * Time.deltaTime), 0, 0);
 						rigidbody.velocity = movement;
-				} else if (Input.GetKeyDown (KeyCode.A) || Input.acceleration.x < -deadzone_width / 2) {
+		} else if (Input.GetKey (KeyCode.A) || Input.acceleration.x < -deadzone_width / 2) {
 						if (rigidbody.transform.position.x < -x_limit) {
 								rigidbody.velocity = Vector3.zero;
 								return;
